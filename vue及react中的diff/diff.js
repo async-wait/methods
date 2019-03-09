07308f1485cbea9903a242b477c5140f80cffef2
async-wait <17735913088@163.com>
2019-03-09 14:12:03 +0800

    有bug

--- vue及react中的diff/diff.js
+++ vue及react中的diff/diff.js
@@ -1,62 +1,65 @@
-const ATTRS = 'ATTRS';  // dom有变化
-const TEXT = 'TEXT';
-const REMOVE = 'REMOVE';
-const REPLACE = 'REPLACE';
-let Index = 0;
-/**
- * 
- * @param {object} oldTree 旧的dom对象
- * @param {object} newTree 新的dom对象
- */
-function diff(oldTree, newTree) {
-    let patches = {};
-    let index = 0;
-    // 递归 比较后的结果放入到补丁包里面
-    walk(oldTree, newTree, index, patches);
-    return patches;
-};
+(function (win) {
+    const ATTRS = 'ATTRS';  // dom有变化
+    const TEXT = 'TEXT';
+    const REMOVE = 'REMOVE';
+    const REPLACE = 'REPLACE';
+    let Index = 0;
+    /**
+     * 
+     * @param {object} oldTree 旧的dom对象
+     * @param {object} newTree 新的dom对象
+     */
+    function diff(oldTree, newTree) {
+        let patches = {};
+        let index = 0;
+        // 递归 比较后的结果放入到补丁包里面
+        walk(oldTree, newTree, index, patches);
+        return patches;
+    };
 
-function diffAttr(oldAttrs, newAttrs) {
-    let patch = {};
-    for (let key in oldAttrs) {
-        if (oldAttrs[key] !== newAttrs[key]) {
-            patch[key] = newAttrs[key];
+    function diffAttr(oldAttrs, newAttrs) {
+        let patch = {};
+        for (let key in oldAttrs) {
+            if (oldAttrs[key] !== newAttrs[key]) {
+                patch[key] = newAttrs[key];
+            }
         }
-    }
-    for (let key in newAttrs) {
-        if (!oldAttrs.hasOwnProperty(key)) {
-            patch[key] = newAttrs[key];
+        for (let key in newAttrs) {
+            if (!oldAttrs.hasOwnProperty(key)) {
+                patch[key] = newAttrs[key];
+            }
         }
-    }
-    return patch;
-};
+        return patch;
+    };
 
-function walk(oldNode, newNode, index, patches) {
-    let currentPatch = [];
-    // 当节点类型相同时，去比较属性是否相同,产生一个属性补丁包 {type: 'ATTRS', attrs: {class: xxxx}}
-    if (typeof oldNode === 'string' && typeof newNode === 'string') {
-        if (oldNode !== newNode) {
-            currentPatch.push({type: TEXT, text: newNode});
+    function walk(oldNode, newNode, index, patches) {
+        let currentPatch = [];
+        // 当节点类型相同时，去比较属性是否相同,产生一个属性补丁包 {type: 'ATTRS', attrs: {class: xxxx}}
+        if (typeof oldNode === 'string' && typeof newNode === 'string') {
+            if (oldNode !== newNode) {
+                currentPatch.push({type: TEXT, text: newNode});
+            }
+        } else if (!newNode) {
+            currentPatch.push({type: REMOVE, index});
+        } else if (oldNode.type === newNode.type) {
+            let attrs = diffAttr(oldNode.props, newNode.props);
+            if (Object.keys(attrs).length > 0) {
+                currentPatch.push({type: ATTRS, attrs});
+            }
+            // 子集比较
+            diffChildren(oldNode.children, newNode.children, index, patches);
+        } else {
+            currentPatch.push({type: REPLACE, newNode});
         }
-    } else if (!newNode) {
-        currentPatch.push({type: REMOVE, index});
-    } else if (oldNode.type === newNode.type) {
-        let attrs = diffAttr(oldNode.props, newNode.props);
-        if (Object.keys(attrs).length > 0) {
-            currentPatch.push({type: ATTRS, attrs});
+        if (currentPatch.length > 0) { // 当前元素确实有补丁
+            patches[index] = currentPatch;
         }
-        // 子集比较
-        diffChildren(oldNode.children, newNode.children, index, patches);
-    } else {
-        currentPatch.push({type: REPLACE, newNode});
-    }
-    if (currentPatch.length > 0) { // 当前元素确实有补丁
-        patches[index] = currentPatch;
-    }
-};
+    };
 
-function diffChildren(oldChildren, newChildren, index, patches) {
-    oldChildren.forEach((item, idx) => {
-        walk(item, newChildren[idx], ++Index, patches);
-    });
-};
+    function diffChildren(oldChildren, newChildren, index, patches) {
+        oldChildren.forEach((item, idx) => {
+            walk(item, newChildren[idx], ++Index, patches);
+        });
+    };
+    win.diff = diff;
+})(window);
--- vue及react中的diff/patch.js
+++ vue及react中的diff/patch.js
@@ -1,3 +1,43 @@
-function patch(el, patches) {
+(function (win) {
+    let index = 0;
+    function patch(node, patches) {
+        console.log(patches);
+        walk(node, patches);
+    }
 
-}
+    function walk(node, patches) {
+        let currentPatch = patches[index++];
+        let childNodes = node.childNodes;
+        childNodes.forEach(item => walk(item, patches));
+        if (currentPatch) {
+            doPatch(node, currentPatch);
+        }
+    }
+
+    function doPatch(node, patch) {
+        patch.forEach(item => {
+            switch (item.type) {
+                case 'ATTRS':
+                    for (let k in item.attrs) {
+                        if (item.attrs[k]) {
+                            node.setAttribute(k, item.attrs[k]);
+                        } else {
+                            node.removeAttribute(k);
+                        }
+                    }
+                    break;
+                case 'REMOVE':
+                    node.parentNode.removeChild(node);
+                    break;
+                case 'TEXT':
+                    node.textContent = item.text;
+                    break;
+                case 'REPLACE':
+                    let newNode = (item.newNode instanceof Element) ? render(item.newNode) : document.createTextNode(item.newNode);
+                    node.parentNode.replaceChild(newNode, node);
+                    break;
+            }
+        });
+    }
+    win.patch = patch;
+})(window);
