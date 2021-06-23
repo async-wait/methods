43bf634d3e45e429890b19b38a93e88a8dc2edb9
fantengjiang <fantengjiang@yunshuxie.com>
2021-06-23 14:52:58 +0800

    实现ne wnew

--- js/api/new.js
+++ js/api/new.js
@@ -2,6 +2,12 @@
  * 实现new
  */
 
-function myNew () {
-  
+function myNew (Fn) {
+  const newObj = Object.create(Fn.prototype);
+  const params = [...arguments].slice(1);
+  const result = Fn.apply(newObj, params);
+  if (result && typeof result === 'object' || typeof result === 'function') {
+    return result;
+  }
+  return newObj;
 }
