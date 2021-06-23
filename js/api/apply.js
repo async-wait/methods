b44b090b68b9ffd20f9dbf06c2fb425f773af4c7
fantengjiang <fantengjiang@yunshuxie.com>
2021-06-23 11:48:37 +0800

    bind实现

--- js/api/apply.js
+++ js/api/apply.js
@@ -2,6 +2,19 @@
  * 实现apply函数
  */
 
-Function.prototype.myApply = function () {
-  
+Function.prototype.myApply = function (context) {
+  if (typeof this !== 'function') {
+    return TypeError('error');
+  }
+
+  context = context || window;
+  context.fn = this;
+  let result = null;
+  if (arguments[1]) {
+    result = context.fn(...arguments[1]);
+  } else {
+    result = context.fn();
+  }
+  delete context.fn;
+  return result;
 }
--- js/api/bind.js
+++ js/api/bind.js
@@ -1,6 +1,17 @@
 /**
  * 实现一个bind函数
  */
-Function.prototype.myBind = function () {
-  
+Function.prototype.myBind = function (context) {
+  if (typeof this !== 'function') {
+    return TypeError('error');
+  }
+
+  const _this = this;
+  const args = [...arguments].slice(1);
+  return function Fn () {
+    if (this instanceof Fn) {
+      return new _this(...args, ...arguments);
+    }
+    return _this.apply(context, args.concat(...arguments));
+  }
 }
